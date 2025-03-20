09cae5c39bbde5d0a02c962c7a94d7c390b05cb6
Abdullah <BCS23044@pugc.edu.pk>
2025-03-20 15:52:51 +0500

    Initial commit

---
+++ lab_01.py
@@ -0,0 +1,24 @@
+# <--- PYTHON SYNTAX --->
+print("Hello, World!")
+
+# <--- COMMENTS --->
+x = 1
+# The initial value of x is 1.
+if x > 0:
+    print("These are two comments")   # Print a string
+
+# <--- INPUT/OUTPUT --->
+txt = input("Type something to test this out: ")
+print(txt)
+
+# <--- MULTIPLE STATEMENTS ON A SINGLE LINE --->
+print("Statement1")
+print("Statement2")
+# You can write above two statements in the following way
+print("Statement1"); print("Statement2")
+
+# <--- INDENTATION --->
+x = 1
+if x > 0:
+print("This statement has no Indenation")
+print("This statement has no Indenation")
